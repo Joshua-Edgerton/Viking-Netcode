9d0660542792653c1f15e0bce90ac330b89b1d68
Joshua Edgerton <43973457+Joshua-Edgerton@users.noreply.github.com>
2023-01-05 21:43:11 -0600

    Normalized movement

--- Assets/Scripts/PlayerNetwork.cs
+++ Assets/Scripts/PlayerNetwork.cs
@@ -1,28 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Netcode;
 using UnityEngine;
 
-public class PlayerNetwork : MonoBehaviour
+public class PlayerNetwork : NetworkBehaviour
 {
     [SerializeField] private float moveSpeed = 3f;
 
     private void Update()
     {
-        if (Input.GetKey(KeyCode.D)) 
-        {
-            transform.position += Vector3.right * moveSpeed * Time.deltaTime;
-        }
-        if (Input.GetKey(KeyCode.A)) 
-        {
-            transform.position += Vector3.right * -moveSpeed * Time.deltaTime;
-        }
-        if (Input.GetKey(KeyCode.W)) 
-        {
-            transform.position += Vector3.up * moveSpeed * Time.deltaTime;
-        }
-        if (Input.GetKey(KeyCode.S)) 
-        {
-            transform.position += Vector3.up * -moveSpeed * Time.deltaTime;
-        }
+        if (!IsOwner) return;
+
+        float moveX = 0f;
+        float moveY = 0f;
+        if (Input.GetKey(KeyCode.W)) moveY = +1f;
+        if (Input.GetKey(KeyCode.S)) moveY = -1f;
+        if (Input.GetKey(KeyCode.A)) moveX = -1f;
+        if (Input.GetKey(KeyCode.D)) moveX = +1f;
+        Vector3 moveDir = new Vector3(moveX, moveY).normalized;
+        transform.position += moveDir * moveSpeed * Time.deltaTime;
     }
 }
